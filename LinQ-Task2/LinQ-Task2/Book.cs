0703db4e0d265b5d8542b68b9fb61ea37415def3
ahmedatef-hu <ahmed8482atef@gmail.com>
2026-03-10 12:43:09 -0700

    Add LinQ-Task2

---
+++ LinQ-Task2/LinQ-Task2/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinQ_Task2
+{
+    internal class Book
+    {
+    }
+}
